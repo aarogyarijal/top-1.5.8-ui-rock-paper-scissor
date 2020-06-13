7ec723c1324440d94010eda35bef94b68c06fc40
aarogyarijal <aarogya.rijal@gmail.com>
2020-06-14 00:46:14 +0545

    Added COmments and fixed image gap

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,72 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0; //Stores Player's Score
+let computerScore = 0; //Stores Computer's Score
 
 
-play();
-
-function play(){
-    console.log("start!")
-    let playerSelectionhc="";
-    let computerSelectionhc="";
-
-    let round = 0;
-    let resultCounter = 0;
+play(); //Function to start playing
 
 
-    let resultclm = document.querySelector("#result1");
-    
-    let compImage = document.querySelector('#comp-img');
-    let playerImage = document.querySelector('#player-img');
+console.log("start!")
+let playerSelectionhc="";  //Player's Selection in Higher Case
+let computerSelectionhc=""; //Computer's Selection in Higher Case
+
+let round = 0;  //Number of rounds played
+let resultCounter = 0;  //Number of results displayed
 
 
-    const selectionsList = document.querySelectorAll(".selections-list")
-    selectionsList.forEach(selections => selections.addEventListener('click', playerPlay));
+let resultclm = document.querySelector("#result1");  //Selecting 1st Column for displaying result
 
-    function playerPlay(e){
-        rawSelection = e.target.id;
-        if(!rawSelection) return;
-        playerSelectionhc=rawSelection.toUpperCase();
-        console.log(`S:${playerSelectionhc}`);
-        changeImageplayer(playerSelectionhc);
-        game();
-    }
+let compImage = document.querySelector('#comp-img');  //Computer's Selection's Image
+let playerImage = document.querySelector('#player-img');  //Player's Selection's Image
 
-    function changeImagecomp(imageof){
-            
-            if(imageof === 'rock') {
-                compImage.setAttribute ('src','./images/rock.png');
-            }else if(imageof === 'paper') {
-                compImage.setAttribute ('src','./images/paper.png'); 
-            }else {
-                compImage.setAttribute ('src','./images/scissor.png');
-            }
-    }
-    function changeImageplayer(imageof){
-        
-        if(imageof === 'ROCK') {
-            playerImage.setAttribute ('src','./images/rock.png');
-        }else if(imageof === 'PAPER') {
-            playerImage.setAttribute ('src','./images/paper.png'); 
-        }else {
-            playerImage.setAttribute ('src','./images/scissor.png');
-        }
+
+const selectionsList = document.querySelectorAll(".selections-list") //Selects all buttons (Rock, Paper, Scissor)
+selectionsList.forEach(selections => selections.addEventListener('click', playerPlay));  //Adds EventListner for all above mentioned buttons
+
+function playerPlay(e) //After one of the options is clicked, this funcion finds out which button was pressed
+{
+    rawSelection = e.target.id;  //Finding which option was selected via ID
+    if(!rawSelection) return;    //If selection is empty, no option is clicked, return
+    playerSelectionhc=rawSelection.toUpperCase(); 
+    console.log(`S:${playerSelectionhc}`);
+    changeImageplayer(playerSelectionhc);  //Changes player Image to current selection
+    game();  //starts game
 }
 
-    function computerPlay(){
+function changeImagecomp(imageof){
+        
+        if(imageof === 'rock') {
+            compImage.setAttribute ('src','./images/rock.png');
+        }else if(imageof === 'paper') {
+            compImage.setAttribute ('src','./images/paper.png'); 
+        }else {
+            compImage.setAttribute ('src','./images/scissor.png');
+        }
+}
+function changeImageplayer(imageof){
+    
+    if(imageof === 'ROCK') {
+        playerImage.setAttribute ('src','./images/rock.png');
+    }else if(imageof === 'PAPER') {
+        playerImage.setAttribute ('src','./images/paper.png'); 
+    }else {
+        playerImage.setAttribute ('src','./images/scissor.png');
+    }
+
+
+function game(){
+    round++;
+    computerSelectionhc = computerPlay(); //Randomly Selects Computer's Move
+
+    let results = play(computerSelectionhc, playerSelectionhc);  //Finds out who won
+    if(results==="Player"){
+        playerScore++;
+        
+    }
+    else if(results==="Computer"){
+        computerScore++;
+    }
+
+    function computerPlay(){  //Randomly Selecting Computer's move
         let randomizedNum = Math.random()*3
 
         if(randomizedNum<=1){
@@ -73,7 +87,7 @@ function play(){
         }
     }
 
-    function play(computerSelectionhc, playerSelectionhc){
+    function play(computerSelectionhc, playerSelectionhc){  
 
         console.log(`selections:Computer->${computerSelectionhc}----Player->${playerSelectionhc}`)
 
@@ -106,41 +120,34 @@ function play(){
         }
 
     }
-    function game(){
-        round++;
-        computerSelectionhc = computerPlay();
-
-        let results = play(computerSelectionhc, playerSelectionhc);
-        if(results==="Player"){
-            playerScore++;
-            
-        }
-        else if(results==="Computer"){
-            computerScore++;
-        }
+    
         
         console.log(`ROUND ${round}--Computer:${computerScore}  Player:${playerScore}`)
 
-        document.getElementById("player").textContent = playerScore;
-        document.getElementById("comp").textContent = computerScore;
+        document.getElementById("player").textContent = playerScore;  //Updates Player Score
+        document.getElementById("comp").textContent = computerScore;  //Updates Computer Score
 
-        if (resultCounter>5){resultclm = document.querySelector("#result2");}
+
+        if (resultCounter>5){resultclm = document.querySelector("#result2");} //Starts displaying results in column 2 after column 1 is filled
        
+        playAgain(); //Asks if you want to play again
 
-        if(computerScore+playerScore===5 && computerScore>playerScore){
-            if(confirm("Computer Wins! Press 'OK' to Play Again.")){
-                location=location;
+        function playAgain(){
+            if(computerScore+playerScore===5 && computerScore>playerScore){
+                if(confirm("Computer Wins! Press 'OK' to Play Again.")){
+                    location=location; //Refreshes Page
+                }
+                else{
+                    return;
+                }
             }
-            else{
-                return;
-            }
-        }
-        else if(computerScore+playerScore===5 && computerScore<playerScore){
-            if(confirm("Hurray! You Win! Press 'OK' to Play Again.")){
-                location=location;
-            }
-            else{
-                return;
+            else if(computerScore+playerScore===5 && computerScore<playerScore){
+                if(confirm("Hurray! You Win! Press 'OK' to Play Again.")){
+                    location=location; //Refreshes Page
+                }
+                else{
+                    return;
+                }
             }
         }
 
